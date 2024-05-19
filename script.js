9fdc63fe0a3b3a98528c75ae10e71e8728d00815
Sara Ahmed <124041622+SaraAhmed34@users.noreply.github.com>
2024-05-19 19:36:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ var quoteInput=document.getElementById('quote');
 var authorInput=document.getElementById('author');
 
 var quets=[
-    quote={
+    quot={
         q:"“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”",
         auther: "― Marilyn Monroe"
     },
- quote={
+ quot={
         q:"“So many books, so little time.”",
         auther: "― Marilyn Monroe"
     },
-      quote={
+      quot={
         q:"“A room without books is like a body without a soul.”",
         auther: "― Marilyn Monroe"
     }
 ]
 
+localStorage.setItem("quete", JSON.stringify(quets));
 var storedQuets = JSON.parse(localStorage.getItem("quete"));
 if(storedQuets !=null){
     quets=storedQuets;
